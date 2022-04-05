512f682a27ad3bc11839c00b14a16c9303baf258
Fernando Orozco <forozcomercado@gmail.com>
2022-04-05 11:06:22 -0500

    Add the routes to render the folder view, and also set up the middleware to upload files.

---
+++ routes/app.routes.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const multiparty = require("connect-multiparty");
+const { Router } = require("express");
+const router = Router();
+const ViewController = require("../controllers/app.controllers");
+
+// This middleware will be use to upload the files to the folders, by default to 'home'
+const multipartyMiddleware = multiparty({
+  uploadDir: path.join(__dirname, "public", "images", "home"),
+});
+
+router.get("/", (req, res) => {
+  res.redirect("/app/home");
+});
+
+router.get("/:folder", ViewController.folderViewController);
+
+module.exports = router;
