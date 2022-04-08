de6a494f7c35e602666f47969456a52c25867120
Fernando Orozco <forozcomercado@gmail.com>
2022-04-08 18:02:59 -0500

    Files upload completed

--- controllers/app.controllers.js
+++ controllers/app.controllers.js
@@ -1,33 +1,75 @@
 const path = require("path");
 const fs = require("fs");
-const FileModel = require("../models/file.model");
+const { FileModel, FolderModel } = require("../models/file.model");
 const pathToHome = path.join(__dirname, "..", "public", "images", "home");
+let FOLDERS = []; // this var will hold the names and IDs of the folders created to avoid ask the db each time
 
-exports.folderViewController = function (req, res) {
+exports.folderViewController = async function (req, res) {
   // if the url contains any space, it will be removed and redirect
   if (/%20/.test(req.url)) {
     let folder = req.url.replace(/%20/g, "").substring(1);
     res.redirect("/app/" + folder);
     return;
   }
-  res.render("folder", { layout: "main", url: req.url.substring(1) });
+  if (FOLDERS.length === 0) {
+    FOLDERS = await FolderModel.find();
+    // console.log(FOLDERS);
+  }
+
+  let urlName = req.url.substring(1);
+  console.log(1, urlName);
+  let folder = FOLDERS.find((folder) => folder.name === urlName);
+  let files = await FileModel.find({ folder_id: folder._id });
+  files = files.map((file) => {
+    file.url = urlName;
+    return file;
+  });
+  // console.log("files", files);
+
+  res.render("folder", {
+    layout: "main",
+    url: urlName,
+    FOLDERS,
+    files,
+  });
 };
 
 exports.uploadFile = async function (req, res) {
   try {
     const file = req.files.file.file;
-    const fileDoc = {};
+    const { url } = req.body;
     await fs.renameSync(file.path, path.join(pathToHome, file.name));
+    const fileDoc = new FileModel();
+
+    // console.log("Extracting url", url);
 
     fileDoc.name = file.name;
     fileDoc.path = path.join(pathToHome, file.name);
     fileDoc.size = file.size;
-    fileDoc.type = file.type;
-    fileDoc.created_at = new Date();
-    fileDoc.folder = req.body.url;
-    console.log(fileDoc, fileDoc.folder);
+    fileDoc.type = file.type.split("/")[0];
+    fileDoc.format = file.type.split("/")[1];
 
-    res.redirect("/app/" + fileDoc.folder);
+    switch (fileDoc.type) {
+      case "image":
+        fileDoc.isImage = true;
+        break;
+      case "video":
+        fileDoc.isVideo = true;
+        break;
+      case "audio":
+        fileDoc.isAudio = true;
+        break;
+    }
+
+    // console.log("Folders", FOLDERS);
+    // finds the folder this file belongs to
+    let folder = FOLDERS.find((folder) => folder.name === url);
+    // console.log("folder found ?", folder);
+    fileDoc.folder_id = folder._id;
+    await fileDoc.save();
+    console.log(fileDoc);
+
+    res.redirect("/app/" + req.body.url);
   } catch (err) {
     console.log(err);
     res.redirect("/app/");
--- models/file.model.js
+++ models/file.model.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const { Schema, model, connect } = require("mongoose");
 
 connect("mongodb://localhost:27017/CloudStorage").then(() => {
@@ -9,8 +10,34 @@ const FileSchema = new Schema({
   path: String,
   size: Number,
   type: { type: String },
-  created_at: Date,
-  folder: String, // save in which folder is the file stored
+  format: { type: String },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  folder_id: mongoose.Types.ObjectId, // save in which folder is the file stored,
+  isVideo: {
+    type: Boolean,
+    default: false,
+  },
+  isImage: {
+    type: Boolean,
+    default: false,
+  },
+  isAudio: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-module.exports = model("File", FileSchema);
+const FolderSchema = new Schema({
+  name: String,
+});
+
+const FileModel = model("File", FileSchema);
+const FolderModel = model("Folder", FolderSchema);
+
+module.exports = {
+  FileModel,
+  FolderModel,
+};
