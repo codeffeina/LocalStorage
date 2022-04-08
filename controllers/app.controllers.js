781f4fa24b867c2c601854e189e126385c055c83
Fernando Orozco <forozcomercado@gmail.com>
2022-04-08 11:43:01 -0500

    Upload file route added, also the route to render the folder view was modified tu cut off the spaces in the url

--- controllers/app.controllers.js
+++ controllers/app.controllers.js
@@ -1,3 +1,34 @@
+const path = require("path");
+const fs = require("fs");
+const FileModel = require("../models/file.model");
+const pathToHome = path.join(__dirname, "..", "public", "images", "home");
+
 exports.folderViewController = function (req, res) {
-  res.render("folder", { layout: "main" });
+  // if the url contains any space, it will be removed and redirect
+  if (/%20/.test(req.url)) {
+    let folder = req.url.replace(/%20/g, "").substring(1);
+    res.redirect("/app/" + folder);
+  }
+  res.render("folder", { layout: "main", url: req.url });
+};
+
+exports.uploadFile = async function (req, res) {
+  try {
+    const file = req.files.file.file;
+    const fileDoc = {};
+    await fs.renameSync(file.path, path.join(pathToHome, file.name));
+
+    fileDoc.name = file.name;
+    fileDoc.path = path.join(pathToHome, file.name);
+    fileDoc.size = file.size;
+    fileDoc.type = file.type;
+    fileDoc.created_at = new Date();
+    fileDoc.folder = req.body.url.substring(1);
+    console.log(fileDoc, fileDoc.folder);
+
+    res.redirect("/app/" + fileDoc.folder);
+  } catch (err) {
+    console.log(err);
+    res.redirect("/app/");
+  }
 };
