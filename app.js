248292a8307671efeab171f2617aa5cbfab264f7
Fernando Orozco <forozcomercado@gmail.com>
2022-04-11 19:14:48 -0500

    now, the first time the app runs, it automatically creates images and images/home folders

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const path = require("path");
+const fs = require("fs");
 const morgan = require("morgan");
 const express = require("express");
 const routes = require("./routes/app.routes");
 const app = express();
 
 // Settings
-
+fs.mkdirSync(path.join(__dirname, "public", "images"));
+fs.mkdirSync(path.join(__dirname, "public", "images", "home"));
 app.set("view engine", "hbs");
 app.set("port", process.env.PORT || 3001);
 
