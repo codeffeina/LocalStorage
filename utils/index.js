a2e864bf4b6d1e94589894686e40c150a79c3c70
Fernando Orozco <forozcomercado@gmail.com>
2022-04-12 09:12:43 -0500

    Utils folder and index file created

---
+++ utils/index.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const fs = require("fs");
+
+module.exports = {
+  pathToImages: path.join(__dirname, "..", "public", "images"),
+  allowTypes: ["audio", "image", "video"],
+  renameTo: function (oldPath, newPath) {
+    fs.renameSync(oldPath, newPath);
+  },
+  exists: function (path) {
+    return fs.existsSync(path);
+  },
+  setType: function (file) {
+    switch (file.type) {
+      case "image":
+        file.isImage = true;
+        break;
+      case "video":
+        file.isVideo = true;
+        break;
+      case "audio":
+        file.isAudio = true;
+        break;
+    }
+  },
+  setFolder: function (FOLDERS, file) {
+    let folder = FOLDERS.find((folder) => folder.name === url);
+    file.folder_id = folder._id;
+  },
+};
