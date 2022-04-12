01c7fac5dc86cb70456544da922323e6b63ff7aa
Fernando Orozco <forozcomercado@gmail.com>
2022-04-12 09:13:07 -0500

    Refactoring the app controllers

--- controllers/app.controllers.js
+++ controllers/app.controllers.js
@@ -1,9 +1,8 @@
 const path = require("path");
 const fs = require("fs");
 const { FileModel, FolderModel } = require("../models/");
-const pathToImages = path.join(__dirname, "..", "public", "images");
+const Utils = require("../utils");
 let FOLDERS = []; // this var will hold the names and IDs of the folders created to avoid ask the db each time
-const AllowTypes = ["audio", "image", "video"];
 
 exports.folderViewController = async function (req, res) {
   // if the url contains any space, it will be removed and redirect
@@ -15,6 +14,7 @@ exports.folderViewController = async function (req, res) {
 
   // find all folders in the db
   FOLDERS = await FolderModel.find().select("name").lean();
+  console.log(FOLDERS);
 
   // extract the folder name from the url
   let urlName = req.url.substring(1);
@@ -48,36 +48,26 @@ exports.uploadFile = async function (req, res) {
     const { url } = req.body;
 
     // check if the type of the file is allowed
-    if (!AllowTypes.includes(file.type.split("/")[0])) {
+    // file.type = "image/jpg", for instance.
+    if (!Utils.allowTypes.includes(file.type.split("/")[0])) {
       fs.rmSync(file.path);
       req.app.locals.showMessage = true;
       res.redirect("/app/" + req.body.url);
       return;
     }
     req.app.locals.showMessage = false;
-    fs.renameSync(file.path, path.join(pathToImages, url, file.name));
+    Utils.renameTo(file.path, path.join(Utils.pathToImages, url, file.name));
 
     fileDoc.name = file.name;
-    fileDoc.path = path.join(pathToImages, url, file.name);
+    fileDoc.path = path.join(Utils.pathToImages, url, file.name);
     fileDoc.size = file.size;
     fileDoc.type = file.type.split("/")[0];
     fileDoc.format = file.type.split("/")[1];
-
-    switch (fileDoc.type) {
-      case "image":
-        fileDoc.isImage = true;
-        break;
-      case "video":
-        fileDoc.isVideo = true;
-        break;
-      case "audio":
-        fileDoc.isAudio = true;
-        break;
-    }
-
+    // set the type of the file
+    Utils.setType(fileDoc);
     // finds the folder this file belongs to
-    let folder = FOLDERS.find((folder) => folder.name === url);
-    fileDoc.folder_id = folder._id;
+    Utils.setFolder(FOLDERS, fileDoc);
+    // saved the document in the database
     await fileDoc.save();
 
     res.redirect("/app/" + url);
@@ -95,7 +85,7 @@ exports.createFolder = async function (req, res) {
       return res.redirect("/app/");
     }
 
-    if (fs.existsSync(path.join(pathToImages, folder))) {
+    if (Utils.exists(path.join(pathToImages, folder))) {
       req.app.locals.showMessageFolder = { state: true, folder };
       res.redirect("/app/" + url);
       return;
