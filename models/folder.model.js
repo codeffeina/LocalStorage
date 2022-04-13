95a1c4aab62f0573604f3cf5e38ccfb3e537d343
Fernando Orozco <forozcomercado@gmail.com>
2022-04-13 14:42:14 -0500

    Fixing folder model

--- models/folder.model.js
+++ models/folder.model.js
@@ -1,8 +1,13 @@
+const { default: mongoose } = require("mongoose");
 const { Schema, model } = require("mongoose");
 
 const FolderSchema = new Schema({
   name: String,
   path: String,
+  files: {
+    type: [mongoose.Types.ObjectId],
+    default: [],
+  },
 });
 
 module.exports = model("Folder", FolderSchema);
