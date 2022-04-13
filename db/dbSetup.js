ddb571919515a6ce09f3353831dc5a89ab8cc050
Fernando Orozco <forozcomercado@gmail.com>
2022-04-13 15:29:20 -0500

    Add method deleteFolderWithoutExistingCollection in the folderRepo

--- db/dbSetup.js
+++ db/dbSetup.js
@@ -1,12 +1,18 @@
 const { FolderModel } = require("../models");
 const Utils = require("../utils");
+const path = require("path");
 const FolderRepo = require("../repositories/folder.repository");
 
 // checks if the home folder exists, if it doesn't it creates it
 async function createCollections() {
   try {
-    if (!(await FolderModel.exists({ name: "home" }))) {
-      await FolderModel.insertMany({ name: "home" });
+    if (!(await FolderRepo.exists({ name: "home" }))) {
+      await FolderModel.insertMany({
+        name: "home",
+        path: path.join(Utils.pathToImages, "home"),
+        files: [],
+      });
+      let homeDir = await FolderRepo.getFolders({ name: "home" }, "name path");
     }
   } catch (error) {
     throw new Error(error);
@@ -26,10 +32,24 @@ async function deleteCollectionWithoutExistingFolder() {
   }
 }
 
+async function deleteFolderWithoutExistingCollection() {
+  try {
+    let folders = Utils.readDirContent(Utils.pathToImages);
+    for (let folder of folders) {
+      if (!(await FolderRepo.exists({ name: folder }))) {
+        Utils.removeDir(Utils.pathToImages, folder);
+      }
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
 exports.setup = async function () {
   try {
     await createCollections();
     await deleteCollectionWithoutExistingFolder();
+    await deleteFolderWithoutExistingCollection();
   } catch (error) {
     throw new Error(error);
   }
