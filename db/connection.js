aff5e600bc5f3a8e42fa6e4cfd9f980a3385c585
Fernando Orozco <forozcomercado@gmail.com>
2022-04-15 16:28:40 -0500

    changed name of the database

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const { connect } = require("mongoose");
 
 async function setupDB() {
   try {
-    await connect("mongodb://localhost:27017/CloudStorage");
+    await connect("mongodb://localhost:27017/LocalStorage");
     console.log("Database connection!");
     await setup();
   } catch (err) {
